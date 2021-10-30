1fcb55f0192293a380752695f5597d6799b6d708
DiosAkhilleus <abertin1809@gmail.com>
2021-10-29 23:16:59 -0500

    added initial routing and links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
+import MSViewer from './Components/MSViewer';
 import './App.css';
 
 function App() {
   return (
-    <Home></Home>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/msviewer' component={MSViewer} />
+      </Switch>
+    </BrowserRouter>
   ); 
 }
 
--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import '../App.css';
 
-const Home: React.FC = () => {
-    return (
-        <div> 
-           Hello World Finally Got it
-        </div>
+interface Props {
+    
+}
+
+const Home = (props: Props) => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to='/msviewer'>Manuscript Viewer</Link>
+    </div>
     )
-} 
+}
 
 export default Home
+
